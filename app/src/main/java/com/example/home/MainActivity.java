e1dc4b95f0cea544ee6680cd9b89dd0a4c91875c
lailaeliotti1 <eliottilaila@gmail.com>
2022-06-23 12:05:44 -0700

    Able to swipe back and forth between login and register activities

--- app/src/main/java/com/example/home/MainActivity.java
+++ app/src/main/java/com/example/home/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.TextView;
+
+import com.example.home.login.LoginActivity;
+import com.example.home.login.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
