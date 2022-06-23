ba7a79be2393e2cbdc3c547f39267cb9f13ca73d
lailaeliotti1 <eliottilaila@gmail.com>
2022-06-23 12:05:44 -0700

    ABle to swipe back and forth between login and register activities

--- app/src/main/java/com/example/home/login/RegisterActivity.java
+++ app/src/main/java/com/example/home/login/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,8 +16,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.home.R;
-//import com.example.home.models.User;
-import com.example.home.models.User;
+//import com.example.home.models._User;
+import com.example.home.models._User;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseUser;
@@ -24,15 +25,16 @@ import com.parse.SignUpCallback;
 
 public class RegisterActivity extends AppCompatActivity {
     public static final String TAG = "Register Activity";
-    private TextView LogoTextView;
-    private TextView mSubtitleTextView;
-    private EditText mUsernameEditText;
-    private EditText mEmailEditText;
-    private EditText mPasswordEditText;
-    private ImageView mBackArrowImageView;
-    private TextView mSwipeLeftTextView;
-    private RelativeLayout mRegisterRelativeLayout;
+    private TextView mtvLogo;
+    private TextView mtvSubtitle;
+    private EditText metUsername;
+    private EditText metEmail;
+    private EditText metPassword;
+    private ImageView mivBackArrow;
+    private TextView mtvSwipeLeft;
+    private RelativeLayout mrvRegister;
     private Button mbtnRegister;
+    public float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +42,15 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
-        LogoTextView = findViewById(R.id.LogoTextView);
-        mSubtitleTextView = findViewById(R.id.SubtitleTextView);
+        mtvLogo = findViewById(R.id.LogoTextView);
+        mtvSubtitle = findViewById(R.id.SubtitleTextView);
         mbtnRegister = findViewById(R.id.RegisterButton);
-        mEmailEditText = findViewById(R.id.EmailEditText);
-        mUsernameEditText = findViewById(R.id.UserNameEditText);
-        mPasswordEditText = findViewById(R.id.PassEditText);
-        mBackArrowImageView = findViewById(R.id.BackArrowImageView);
-        mRegisterRelativeLayout = findViewById(R.id.RegisterRelativeLayout);
-        mSwipeLeftTextView = findViewById(R.id.SwipeLeftTextView);
+        metEmail = findViewById(R.id.EmailEditText);
+        metUsername = findViewById(R.id.UserNameEditText);
+        metPassword = findViewById(R.id.PassEditText);
+        mivBackArrow = findViewById(R.id.BackArrowImageView);
+        mrvRegister = findViewById(R.id.RegisterRelativeLayout);
+        mtvSwipeLeft = findViewById(R.id.SwipeLeftTextView);
 
         if(ParseUser.getCurrentUser() !=null ) {
             goLoginActivity();
@@ -58,25 +60,32 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.i(TAG, "onClick login button");
                     //goLoginActivity();
-                    String userName= setUserName(mUsernameEditText);
-                    String password = setPassword(mPasswordEditText);
-                    String email = setEmail(mEmailEditText);
+                    String userName = metUsername.getText().toString();
+                    String password = metPassword.getText().toString();
+                    String email = metEmail.getText().toString();
+
                     // checking if the entered text is empty or not.
-                    if (TextUtils.isEmpty(userName) || TextUtils.isEmpty(password)) {
-                        Toast.makeText(RegisterActivity.this, getString(R.string.please_enter_username_and_password), Toast.LENGTH_SHORT).show();
+                    if (TextUtils.isEmpty(userName) && TextUtils.isEmpty(password)) {
+                        Toast.makeText(RegisterActivity.this, "Please enter user name and password", Toast.LENGTH_SHORT).show();
                     }
+
                     // calling a method to register a user.
                     registerUser(userName, password, email);
+
+
             }
         });
     }
 
     private void registerUser (String userName, String password, String email){
-        User user = new User();
+        _User user = new _User();
+
         // Set the user's username and password,
         // which can be obtained from edit text
         user.setUsername(userName);
+        Log.i(TAG, metUsername.getText().toString());
         user.setPassword(password);
+        Log.i(TAG, metPassword.getText().toString());
         user.setEmail(email);
 
         // calling a method to register the user.
@@ -90,6 +99,7 @@ public class RegisterActivity extends AppCompatActivity {
                     // redirecting our user to new activity and passing the user name.
                     Toast.makeText(RegisterActivity.this, "User Registered successfully", Toast.LENGTH_SHORT).show();
                     goLoginActivity();
+                    //i.putExtra("username", userName);
                 } else {
                     // if we get any error then we are logging out
                     // our user and displaying an error message
@@ -101,12 +111,27 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+
     private void goLoginActivity(){
         Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
         startActivity(i);
         finish();
     }
-    private String setUserName(EditText userName) {return userName.getText().toString();}
-    private String setPassword(EditText password) {return password.getText().toString();}
-    private String setEmail(EditText email) {return email.getText().toString();}
+    public boolean onTouchEvent(MotionEvent touchevent){
+        switch(touchevent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if(x1 < x2){
+                    Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(i);
+                }
+                break;
+        }
+        return false;
+    }
 }
