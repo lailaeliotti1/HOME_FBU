4d7b59aa19e75ba257ceab0ee484d7099ed8a725
lailaeliotti1 <eliottilaila@gmail.com>
2022-06-23 12:05:44 -0700

    ABle to swipe back and forth between login and register activities

--- app/src/main/java/com/example/home/MainActivity.java
+++ app/src/main/java/com/example/home/MainActivity.java
@@ -1,47 +1,23 @@
 package com.example.home;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
-import com.example.home.preferences.PreferenceFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.home.login.LoginActivity;
+import com.example.home.login.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "Main Activity";
-    final FragmentManager fragmentManager = getSupportFragmentManager();
-    private BottomNavigationView bottomNavigationView;
-    private PreferenceFragment preferenceFragment = new PreferenceFragment();
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment;
-                switch (item.getItemId()) {
-                    case R.id.action_preferences:
-                        fragment = preferenceFragment;
-                        break;
-                    default:
-                        fragment = preferenceFragment;
-                        break;
-                }
-                fragmentManager.beginTransaction().replace(R.id.fragmentContainerView, fragment).commit();
-                return true;
-            }
-        });
-
+        textView = findViewById(R.id.textView);
 
 
     }
