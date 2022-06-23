bcd719a2f0c09d6892c62b432e4a71b4ffe0173d
lailaeliotti1 <eliottilaila@gmail.com>
2022-06-23 16:28:49 -0700

    Logout button created and bottomnavigation bar with preference fragment

--- app/src/main/java/com/example/home/preferences/PreferenceFragment.java
+++ app/src/main/java/com/example/home/preferences/PreferenceFragment.java
@@ -5,36 +5,21 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
+
 import com.example.home.R;
 import com.example.home.login.LoginActivity;
 import com.parse.ParseUser;
 
 
 public class PreferenceFragment extends Fragment {
-    private ConstraintLayout mPrefConLayout;
-    private TextView mPreferenceTextView;
+
     private Button mLogoutButton;
-    private TextView mBedTextView;
-    private Spinner mSpinnerBedrooms;
-    private TextView mBathTextView;
-    private Spinner mSpinnerBathrooms;
-    private TextView mHouseStyleTextView;
-    private Spinner mSpinnerHouse;
-    private TextView mBudgetTextView;
-    private Spinner mSpinnerBudget;
 
     public PreferenceFragment() {
         // Required empty public constructor
@@ -63,28 +48,6 @@ public class PreferenceFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mPrefConLayout = view.findViewById(R.id.prefConLayout);
-        mPreferenceTextView =  view.findViewById(R.id.PreferenceTextView);
-
-        mBedTextView = view.findViewById(R.id.BedTextView);
-        mSpinnerBedrooms = view.findViewById(R.id.spinBed);
-        ArrayAdapter<CharSequence>bedAdapter=ArrayAdapter.createFromResource(getContext(), R.array.NoOfBedrooms, android.R.layout.simple_spinner_item);
-        bedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        mSpinnerBedrooms.setAdapter(bedAdapter);
-
-        mBathTextView = view.findViewById(R.id.BathTextView);
-        mSpinnerBathrooms = view.findViewById(R.id.spinBath);
-        ArrayAdapter<CharSequence>bathAdapter=ArrayAdapter.createFromResource(getContext(), R.array.NoOfBathrooms, android.R.layout.simple_spinner_item);
-        bathAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        mSpinnerBathrooms.setAdapter(bathAdapter);
-
-        mHouseStyleTextView = view.findViewById(R.id.HouseStyleTextView);
-        mSpinnerHouse = view.findViewById(R.id.spinHouse);
-        ArrayAdapter<CharSequence>locationAdapter=ArrayAdapter.createFromResource(getContext(), R.array.houseStyle, android.R.layout.simple_spinner_item);
-        bedAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        mSpinnerBedrooms.setAdapter(bedAdapter);
-
-
         mLogoutButton = view.findViewById(R.id.LogoutButton);
         mLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
